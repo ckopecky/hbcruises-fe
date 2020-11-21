f3e5aee6ed884326006f98daa1bd54881ebafe51
ckopecky <kopecky12112@gmail.com>
2020-11-21 00:03:00 -0800

    example query from backend

--- src/App.js
+++ src/App.js
@@ -1,3 +1,23 @@
+// query Query($weatherZipCode: String) {
+//   weather(zipCode: $weatherZipCode) {
+//     city
+//     temperature
+//     conditions
+//     description
+//     feels_like
+//     temp_hi
+//     temp_low
+//     humidity
+//     wind_speed
+//     error
+//     message
+//   }
+// }
+
+// {
+//   "weatherZipCode": "95126"
+// }
+
 const WEATHER =  gql`
     query getWeather($zip: String) {
       weather(zip: $zip) {
