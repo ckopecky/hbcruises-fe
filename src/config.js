7b028d7304ea9fdedc63aee3a3f9afe9826f05cc
ckopecky <kopecky12112@gmail.com>
2020-11-22 18:18:43 -0800

    create storage file for constants

---
+++ src/config.js
@@ -0,0 +1,3 @@
+export const errorPic = 'https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/120270024_10102925663988191_8533342968999125116_o.jpg?_nc_cat=101&ccb=2&_nc_sid=09cbfe&_nc_ohc=fcYRNg-WC8sAX9W424o&_nc_ht=scontent-sjc3-1.xx&oh=8ca770359a4373c9cada4751a1b224b7&oe=5FDDA7D6'
+
+export const iconPicURL = "http://openweathermap.org/img/wn/"
